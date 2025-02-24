f5ed40dab1964de2416d13835ea58496ea464193
Daniel <daniel.mutembei@student.moringaschool.com>
2025-02-24 11:19:31 +0300

    done

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,17 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    def depth_first_traversal(node):
+        if node.get('id') == id:
+          return node
+          
+        for child in node.get('children', []):
+            result = depth_first_traversal(child)
+            if result:
+              return result
+            
+        return None
+
+    if self.root:
+      return depth_first_traversal(self.root)
+    return None
